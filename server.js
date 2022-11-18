7dc8534004b57341adf4e4df259dd7e3ba6f1a49
KimJoonSung <kjs990114@naver.com>
2022-11-19 01:18:25 +0900

    local storage로 간단한 로그인 기능 구현.

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+let user = [{ name: "김준성", nickname: "hide on bush", id: "2018312075", depart: "소프트웨어학과" }];
+let user_count = 1;
+app.post('/connect', (req, res) => {
+    user.push(req.body);
+    user_count++;
+    console.log(JSON.stringify(user))
+    res.send(200);
+})
 app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
+
+
